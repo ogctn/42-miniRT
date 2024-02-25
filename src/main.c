d2d77f27783b3633edb74fbd667b9dba8753f914
oguz <oguzalpcetin@marun.edu.tr>
2024-02-25 19:57:53 +0300

    [ADD]:only opens new window

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgundogd <sgundogd@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ogcetin <ogcetin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 14:32:43 by sgundogd          #+#    #+#             */
-/*   Updated: 2024/02/25 19:22:16 by sgundogd         ###   ########.fr       */
+/*   Updated: 2024/02/25 19:57:17 by ogcetin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,7 @@ int	main(int ac, char **av)
 	if (ac != 2 || control_extension(av[1]))
 		return (printf("Error: Wrong argument"), 1);
 	initialize(&data, av[1]);
-	printf("A: %f %d\n",data.ambient_light->brightness, data.ambient_light->color.r);
-	printf("C: %f,%f\n",data.cam->origin.x, data.cam->fov);
-	printf("%d\n",data.obj_count);
-	printf("sp: %d and %d\n",data.obj_set[0].type, data.obj_set[1].type);
+	main_loop(&data);
+	return (0);
 }
 
-//int main(int ac, char **av) {
-//	t_data	data;
-//
-//	if (ac != 2 || control_extension(av[1]))
-//		return (printf("Error: Wrong argument"), 1);
-//	initialize(&data, av[1]);
-//
-//	unsigned short k = -1;
-//	int i = -1;	while (++i < sphere_count) set_sphere(&genel, &sphere_set[i], ++k);
-//	//	i = -1;	while (++i < plane_count) set_plane(&genel, &plane_set[i], ++k);
-//
-//
-//	set_stuffs(&data);
-//
-//	main_loop(&data);
-//	//mlx_stuffs( &genel);
-//
-//}
